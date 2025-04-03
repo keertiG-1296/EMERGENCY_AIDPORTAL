063a8dee75e71103dfa5805e37f131d7162d9d3e
keertiG-1296 <keerti.s.g2020@gmail.com>
2025-04-03 09:21:06 +0530

    Update config.py

--- EMERGENCY_AIDPORTAL/config.py
+++ EMERGENCY_AIDPORTAL/config.py
@@ -4,7 +4,7 @@ class Config:
     # Database configuration
     DB_HOST = os.environ.get('DB_HOST', 'localhost')
     DB_USER = os.environ.get('DB_USER', 'root')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD', '@$XB.9tAUnsF94j')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD', 'Password')
     DB_NAME = os.environ.get('DB_NAME', 'emergency_aidportal')
     
     # Flask configuration
